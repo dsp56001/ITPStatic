ff8d7b691e60db23e89885e260f3b7ed984f1fa7
Jeff Meyers <jmeyers@colum.edu>
2020-11-03 19:53:26 -0600

    adds my static demo

---
+++ ConsoleAppStatic/ConsoleAppStatic/Dog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppStatic
+{
+    class Dog
+    {
+        //Properties
+        public string Name;
+        public int Age;
+        public int Weight;
+        public int BarkCount;
+        public string BarkSound;
+
+        //This property belongs to the Dog class and not an instance of Dog
+        public static int TotalBarks;
+    
+        //Constructor
+        public Dog()
+        {
+            this.Name = "fido";
+            this.Age = 3;
+            this.Weight = 6;
+            this.BarkSound = "woof";
+        }
+
+        public string Bark()
+        {
+            this.BarkCount++;
+            Dog.TotalBarks++;
+            return this.BarkSound;
+        }
+    }
+}
---
+++ ConsoleAppStatic/ConsoleAppStatic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleAppStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            Console.WriteLine(d.Bark());
+            Console.WriteLine(d.BarkCount);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(d.Bark());
+            }
+            Console.WriteLine(d.BarkCount);
+
+            Dog milo = new Dog() { Name = "milo", BarkSound="arf!!!" };
+            Console.WriteLine(milo.BarkCount);
+            Console.WriteLine(milo.Bark());
+            Console.WriteLine(milo.BarkCount);
+
+            Console.WriteLine("Total:" + Dog.TotalBarks); 
+
+            Console.ReadKey();
+        }
+    }
+}
